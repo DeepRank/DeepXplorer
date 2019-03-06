a942cce27ca806e30835fe85c8fbb5b814ae068a
Nicolas Renaud <n.renaud@tudelft.nl>
2019-03-06 11:40:59 +0100

    added support for map on the fly and clean up tmp datafiles

--- DeepXplorer.py
+++ DeepXplorer.py
@@ -2,5 +2,12 @@
 
 from h5xplorer.h5xplorer import h5xplorer
 import menu
+import glob
+import shutil
 
-app = h5xplorer(menu.context_menu,extended_selection=True)
+app = h5xplorer(menu.context_menu,extended_selection=False)
+
+# clear
+# dirs = glob.glob('./_tmp_*')
+# for d in dirs:
+#     shutil.rmtree(d)
